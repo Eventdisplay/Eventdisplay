8906dd9134b7be90a3397e4f9fe3ef182171c094
GernotMaier <gernot.maier@desy.de>
2019-12-02 21:46:52 +0100

    laser script cleanup

--- scripts/VTS/helper_scripts/ANALYSIS.evndisp_laser_sub.sh
+++ scripts/VTS/helper_scripts/ANALYSIS.evndisp_laser_sub.sh
@@ -2,7 +2,7 @@
 # script to analyse laser files 
 
 # set observatory environmental variables
-source $EVNDISPSYS/setObservatory.sh VTS
+source "$EVNDISPSYS"/setObservatory.sh VTS
 
 # parameters replaced by parent script using sed
 RUN=RUNFILE
@@ -11,7 +11,7 @@ LOGDIR=LOGDIRECTORY
 echo $RUN
 
 # run eventdisplay
-rm -f $LOGDIR/$RUN.laser.log
-$EVNDISPSYS/scripts/VTS/SPANALYSIS.evndisp_laser_run.sh $RUN $TELTOANA &> $LOGDIR/$RUN.laser.log
+rm -f "$LOGDIR/$RUN.laser.log"
+"$EVNDISPSYS"/scripts/VTS/SPANALYSIS.evndisp_laser_run.sh $RUN $TELTOANA &> "$LOGDIR/$RUN.laser.log"
 
 exit
