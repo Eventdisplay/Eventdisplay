23faf401229b50a4fcd72414ca3e754308b8b836
GernotMaier <gernot.maier@desy.de>
2019-12-12 14:18:27 +0100

    fixed lookup table smoothing

--- scripts/VTS/helper_scripts/IRF.lookup_table_combine_sub.sh
+++ scripts/VTS/helper_scripts/IRF.lookup_table_combine_sub.sh
@@ -12,4 +12,7 @@ ODIR=OUTPUTDIR
 # combine the tables
 "$EVNDISPSYS"/bin/combineLookupTables "$ODIR/$FLIST" "$ODIR/$OFILE.root" &> "$ODIR/$OFILE.log"
 
+# smooth lookup tables
+"$EVNDISPSYS"/bin/smoothLookupTables "$ODIR/$OFILE.root" "$ODIR/$OFILE-smoothed.root" &> "$ODIR/$OFILE-smoothed.log"
+
 exit
