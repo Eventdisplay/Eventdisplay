13996fdc686dd980cada36707bc2379be6c0e0c0
GernotMaier <gernot.maier@desy.de>
2021-09-21 15:28:25 +0200

    set eventdisplay version by hand

--- set_v500.sh
+++ set_v500.sh
@@ -10,6 +10,7 @@ if [[ $# < 1 ]]; then
    echo "   e.g., TS, NN"
 fi
 
+EDVERSION="v505"
 USERAFSDIR="/afs/ifh.de/group/cta/scratch/$USER"
 USERLUSTDIR="/lustre/fs23/group/veritas/users/$USER"
 GROUPLUSTDIR="/lustre/fs23/group/veritas"
@@ -27,21 +28,19 @@ export VBFSYS=/afs/ifh.de/group/cta/VERITAS/software/VBF-0.3.4-SL6
 export PATH=$PATH:${VBFSYS}/bin/
 LD_LIBRARY_PATH=$VBFSYS/lib:$LD_LIBRARY_PATH; export LD_LIBRARY_PATH
 
-
-
 export EVNDISPSYS=${TDIR}
 export SOFASYS=${EVNDISPSYS}/sofa
 export LD_LIBRARY_PATH=${EVNDISPSYS}/obj:${LD_LIBRARY_PATH}
-EDVERSION=`$EVNDISPSYS/bin/evndisp --version | tr -d .`
 
 ########################################################################
 # data and IRF directories
 
 # data directory (VBF files)
+export VERITAS_ANALYSIS_TYPE=${1}
 export EVNDISPSCRIPTS=${USERAFSDIR}/EVNDISP/EVNDISP-400/GITHUB_Eventdisplay/Eventdisplay_AnalysisScripts_VTS/scripts/
 export VERITAS_DATA_DIR=/lustre/fs24/group/veritas/
 export VERITAS_USER_DATA_DIR=${USERLUSTDIR}
-export VERITAS_EVNDISP_AUX_DIR=${VERITAS_USER_DATA_DIR}/analysis/Results/${EDVERSION}/${1}/Eventdisplay_AnalysisFiles/
+export VERITAS_EVNDISP_AUX_DIR=${VERITAS_USER_DATA_DIR}/analysis/Results/${EDVERSION}/${VERITAS_ANALYSIS_TYPE}/Eventdisplay_AnalysisFiles/
 export VERITAS_IRFPRODUCTION_DIR=${VERITAS_USER_DATA_DIR}/analysis/Results/
 export VERITAS_USER_LOG_DIR=${USERAFSDIR}/LOGS/VERITAS/
 
