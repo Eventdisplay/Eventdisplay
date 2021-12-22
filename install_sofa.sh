842ac33e94498164639e2e1161e2f025daca41e6
Gernot Maier <gernot.maier@desy.de>
2021-12-22 08:13:40 +0100

    Continous integration (#110)
    Integrate basic github action for branch merging

--- install_sofa.sh
+++ install_sofa.sh
@@ -5,8 +5,11 @@
 # see https://www.iausofa.org for a description
 #
 #
+set -e
 
 echo "Installation of sofa into $EVNDISPSYS "
+
+[[ "$1" ]] && DOWNL=$1 || DOWNL=""
 
 CURDIR=`pwd`
 cd "$EVNDISPSYS"
@@ -20,19 +23,24 @@ then
     exit
 fi
 
-mkdir sofa
+mkdir -p sofa
 cd sofa
 
 # get sofa package from the web page and install
 SOFAD="20210512"
 SOFA="sofa_c-${SOFAD}.tar.gz"
-wget --no-check-certificate https://www.iausofa.org/2021_0512_C/${SOFA}
+if [[ $DOWNL == "CI" ]]; then
+    wget https://syncandshare.desy.de/index.php/s/jr9NrbWFR5MLaDf/download
+    mv -f download ${SOFA}
+else
+    wget --no-check-certificate https://www.iausofa.org/2021_0512_C/${SOFA}
+fi
 if [ ! -e ${SOFA} ]
 then
     echo "error in downloading sofa package"
     exit
 fi
-tar -xvzf ${SOFA}
+tar -xzf ${SOFA}
 rm -f ${SOFA}
 
 ##########################
